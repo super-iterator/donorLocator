6eabc2e25cc3287c593d87cdcfb56ef9cda62d9f
BL <super.iterator@gmail.com>
2016-09-17 09:15:11 +0300

    First push

---
+++ client/app/documentation/docs.js
@@ -0,0 +1,27 @@
+function markdownString() {
+    return `
+
+
+    This project's main purpose is to connect blood donors to patiens who need them, where each donor goes to donors tab in the web app, registers his/her contact info. and blood group along with donor's IP address and geographic location.
+
+    On the main page, patients now can view them across the map, click on donor's mark located in some geographic area.
+
+    Seaching a specific blood group is possible as well, with instant real-time results, you can find all donors with the required blood group, and navigate to their location with a click of a button.
+
+    This project emplolys a set of technologies to empower this project:
+
+    Redux is both a design pattern and a framework for React (and other frameworks as well) has been employed to make separation of concerns while designing the front end React app, that not everyone is modifying the state and turn everything into a mess when we want to track things down and know what happened where
+
+    Technologies used in this project
+
+    - ReactJS
+    - Redux
+    - Meteor
+    - LeafletJS / ESRI-Leaflet (for ArchGIS maps)
+    - Heroku hosting
+    `
+}
+
+
+
+export default markdownString
